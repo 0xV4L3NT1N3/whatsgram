45f39556f23ae1905a97f76e31f8d4171dbb11a8
V4L3NT1N3 <33112835+V4L3NT1N3@users.noreply.github.com>
2020-12-12 04:01:53 +0800

    Added Bottom Nav Bar
    Disabled default text , which worked by restarting the app :')

--- lib/main.dart
+++ lib/main.dart
@@ -9,43 +9,48 @@ void main() {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(70),
-          child: AppBar(
-            title: Text(
-              'Whatsgram',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 38,
-                  fontWeight: FontWeight.normal,
-                  fontFamily: 'Billabong'),
+        appBar: AppBar(
+          title: Text(
+            'Whatsgram',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 33,
+                fontWeight: FontWeight.normal,
+                fontFamily: 'Billabong'),
+          ),
+          actions: [
+            IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
             ),
-            actions: [
-              IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.more_vert_rounded,
+                color: Colors.black,
               ),
-              IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.more_vert_rounded,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-            backgroundColor: Colors.white,
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(
-            Icons.chat,
-            color: Colors.black,
-          ),
+            ),
+          ],
           backgroundColor: Colors.white,
+        ),
+        body: Center(child: Text('Hello World')),
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.call),
+              label: 'Business',
+            ),
+          ],
+          selectedItemColor: Colors.black,
         ),
       ),
     ),
