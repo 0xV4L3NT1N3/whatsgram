04e1780a2d208644d1a3e10ecb7fee0fcddea424
V4L3NT1N3 <33112835+V4L3NT1N3@users.noreply.github.com>
2020-12-14 16:12:21 +0800

    Added Profile page

--- lib/profile.dart
+++ lib/profile.dart
@@ -8,6 +8,59 @@ class Profile extends StatefulWidget {
 class _CallsState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Center(child: Text('Profile Page')));
+    return Column(
+      children: [
+        //Profile picture
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: CircleAvatar(
+            radius: 80,
+          ),
+        ),
+        //Preferences
+        Column(
+          children: [
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text(
+                'Joe',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('3 minutes ago'),
+              trailing: Icon(Icons.edit),
+            ),
+            Divider(
+              indent: 70,
+              height: 5,
+            ),
+            ListTile(
+              leading: Icon(Icons.info_outline),
+              title: Text(
+                'About',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('Doge is your best bet !'),
+              trailing: Icon(Icons.edit),
+            ),
+            Divider(
+              indent: 70,
+              height: 5,
+            ),
+            ListTile(
+              leading: Icon(Icons.call),
+              title: Text(
+                'Phone',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('3 minutes ago'),
+            ),
+            Divider(
+              indent: 70,
+              height: 5,
+            )
+          ],
+        ),
+      ],
+    );
   }
 }
